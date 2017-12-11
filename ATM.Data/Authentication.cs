13b50490006152b0973b80f12bb1879e3fb88547
Rafael Lunardelli <rafael+github@lunardelli.me>
2017-12-11 15:47:58 -0700

    Login

--- ATM.Data/Authentication.cs
+++ ATM.Data/Authentication.cs
@@ -7,15 +7,22 @@ namespace ATM.Data
     {
         public static User Login(string accountNumber, string password)
         {
-            var db = Database.GetInstance();
-            var users = db.GetUsers();
+            //var db = Database.GetInstance();
+            //var users = db.GetUsers();
 
-            var user = users.First(u => u.CheckingAccount.Number.Equals(accountNumber) && u.Password.Equals(password));
+            //var user = users.First(u => u.CheckingAccount.Number.Equals(accountNumber) && u.Password.Equals(password));
 
-            if (user == null)
+            //if (user == null)
+            //{
+            //    throw new AuthenticationException("Account number or password invalid");
+            //}
+
+            if (accountNumber != "123456" || accountNumber == null || password != "secret" || password == null)
             {
                 throw new AuthenticationException("Account number or password invalid");
             }
+
+            var user = new User();
 
             user.Logged = true;
             return user;
---
+++ ATM.UI/Model/LoginModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Input;
+using Windows.UI.Popups;
+using ATM.Data;
+using ATM.Data.Exceptions;
+
+namespace ATM.UI.Model
+{
+    public class LoginModel : ObservableObject
+    {
+        private readonly INavigationService _navigationService;
+        private string _accountNumber;
+        private string _password;
+
+        public string AccountNumber
+        {
+            get => _accountNumber;
+            set
+            {
+                _accountNumber = value;
+                RaisePropertyChangedEvent("AccountNumber");
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                RaisePropertyChangedEvent("Password");
+            }
+        }
+
+        public ICommand LogInCommand => new DelegateCommand(LogIn);
+
+
+        public LoginModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        private async void LogIn(object obj)
+        {
+            // TODO: Verify Account Number and Password 
+
+            try
+            {
+                var user = Authentication.Login(AccountNumber, Password);
+            }
+            catch (AuthenticationException e)
+            {
+                var dialog = new MessageDialog(e.Message);
+                await dialog.ShowAsync();
+            }
+            
+        }
+    }
+}
